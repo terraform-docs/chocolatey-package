d689222a45991f9ec0aca6b6805f3297c52fd1e3
Khosrow Moossavi <khos2ow@gmail.com>
2020-10-13 17:44:01 -0400

    Upgrade package to v0.10.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop'
 
 $packageName = 'terraform-docs'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$checksum = '6eaecdfa79ce8eb9231aaf9f0a40bf0b0b19cac0f4cd49f4428d7e0f636ee85e'
-$url = 'https://github.com/terraform-docs/terraform-docs/releases/download/v0.10.0/terraform-docs-v0.10.0-windows-amd64.exe'
+$checksum = 'ec07ddd6cab4a8f595a91a025345028197dfba2bce40eebdf8d63e8d4e0ac4de'
+$url = 'https://github.com/terraform-docs/terraform-docs/releases/download/v0.10.1/terraform-docs-v0.10.1-windows-amd64.exe'
 $destination = "$toolsPath\terraform-docs.exe"
 
 Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $destination -Url64bit $url -Checksum64 $checksum -ChecksumType64 sha256
